6290afdde125b445679c872012a84c17ea320e2f
l8kout <sqet123@gmail.com>
2023-04-17 23:30:50 +0300

    master 0

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import {Users} from "./components/Users"
-import {useState} from "react";
-import {UserPosts} from "./components/UserPosts";
-
 const App = () => {
-    const [userId, setUserId] = useState(null);
 
     return (
         <div>
-            <Users setUserId={setUserId}/>
 
-            {
-                userId && <UserPosts userId={userId}/>
-            }
         </div>
     );
 };
 
 export default App;
-
-
