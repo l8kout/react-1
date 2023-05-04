f4ef283ac40577e634842ba3b4c15cf5e274d1ac
l8kout <sqet123@gmail.com>
2023-05-04 23:50:56 +0300

    hw-4 -1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import {Link} from "react-router-dom";
+
 const App = () => {
     return (
         <div>
+            <h2>menu</h2>
+            <ul>
+                <li>
+                    <Link to={}>home</Link>
+                </li>
 
+            </ul>
 
         </div>
     );
 };
 
-export default App; 
+export default App;
